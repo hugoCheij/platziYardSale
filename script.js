6858d071642924c4628200618faad602ebe4f3e3
polafranco <polafranco77@gmail.com>
2023-05-01 21:45:16 -0300

    Fusion de vistas menu movil terminada

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 const menuEmail = document.querySelector('.navbar-email');
 const menuEscritorio = document.querySelector('.menu-escritorio');
+const iconoMenuMovil = document.querySelector('.menu');
+const menuMovil = document.querySelector('.menu-movil');
 
 menuEmail.addEventListener('click', cambiarMenuEscritorio);
+iconoMenuMovil.addEventListener('click', camibarMenuMovil);
 
 function cambiarMenuEscritorio() {
     menuEscritorio.classList.toggle('inactive');
 }
+
+function camibarMenuMovil() {
+    menuMovil.classList.toggle('inactive');
+}
+
+
