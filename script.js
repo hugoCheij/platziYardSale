8ed7d79f17a6fe73432a1313435c9c2ccd102852
polafranco <polafranco77@gmail.com>
2023-05-08 23:26:12 -0300

    Carrito de compras en escritorio y movil funcionado

--- script.js
+++ script.js
@@ -2,16 +2,47 @@ const menuEmail = document.querySelector('.navbar-email');
 const menuEscritorio = document.querySelector('.menu-escritorio');
 const iconoMenuMovil = document.querySelector('.menu');
 const menuMovil = document.querySelector('.menu-movil');
+const iconoMenuCarrito = document.querySelector('.navbar-shopping-card');
+const aside = document.querySelector('.product-detail');
 
 menuEmail.addEventListener('click', cambiarMenuEscritorio);
-iconoMenuMovil.addEventListener('click', camibarMenuMovil);
+iconoMenuMovil.addEventListener('click', cambiarMenuMovil);
+iconoMenuCarrito.addEventListener('click', cambiarCarritoAside);
 
 function cambiarMenuEscritorio() {
+    const carritoCerrado = aside.classList.contains('inactive');
+
+    if (!carritoCerrado) {
+        aside.classList.add('inactive');
+    }
+
     menuEscritorio.classList.toggle('inactive');
 }
 
-function camibarMenuMovil() {
+function cambiarMenuMovil() {
+    const carritoCerrado = aside.classList.contains('inactive');
+
+    if (!carritoCerrado) {
+        aside.classList.add('inactive');
+    }
+
     menuMovil.classList.toggle('inactive');
+}
+
+function cambiarCarritoAside() {
+    const menuMovilCerrado = menuMovil.classList.contains('inactive');
+    const menuEscritorioCerrado = menuEscritorio.classList.contains('inactive');
+
+    if (!menuEscritorioCerrado) {
+        menuEscritorio.classList.add('inactive');
+    }
+
+    if (!menuMovilCerrado) {
+        menuMovil.classList.add('inactive');
+    }
+
+    aside.classList.toggle('inactive');
+    
 }
 
 
