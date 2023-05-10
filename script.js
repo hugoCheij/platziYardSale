ddedac5d5e529cc0c5c998177b409d39b1bdef58
polafranco <polafranco77@gmail.com>
2023-05-10 20:33:28 -0300

    mostrando lista de articulos con maquetacion desde JavaScript

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ const iconoMenuMovil = document.querySelector('.menu');
 const menuMovil = document.querySelector('.menu-movil');
 const iconoMenuCarrito = document.querySelector('.navbar-shopping-card');
 const aside = document.querySelector('.product-detail');
+const cardsContainer = document.querySelector('.cards-container');
 
 menuEmail.addEventListener('click', cambiarMenuEscritorio);
 iconoMenuMovil.addEventListener('click', cambiarMenuMovil);
@@ -45,4 +46,61 @@ function cambiarCarritoAside() {
     
 }
 
+const listaProductos = [];
 
+listaProductos.push({
+    nombre: 'Articulo',
+    precio: 120,
+    imagen: 'https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+listaProductos.push({
+    nombre: 'Articulo',
+    precio: 120,
+    imagen: 'https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+listaProductos.push({
+    nombre: 'Articulo',
+    precio: 120,
+    imagen: 'https://images.pexels.com/photos/337909/pexels-photo-337909.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
+});
+
+function crearArticulos(arr) {
+    for (producto of arr) {
+        const productCard = document.createElement('article');
+        productCard.classList.add('card-product');
+    
+        const productImg = document.createElement('img');
+        productImg.setAttribute('src', producto.imagen);
+    
+        const productInfo = document.createElement('section');
+        productInfo.classList.add('info-product');
+    
+        const productSection = document.createElement('section'); 
+    
+        const productPrice = document.createElement('p');
+        productPrice.innerText = '$' + producto.precio;
+        const productName = document.createElement('p');
+        productName.innerText = producto.nombre;
+    
+        productSection.appendChild(productPrice);
+        productSection.appendChild(productName);
+    
+        const productFigure = document.createElement('figure');
+        const productImgCart = document.createElement('img');
+        productImgCart.setAttribute('src', './estilos/iconos/bt_add_to_cart.svg');
+    
+        productFigure.appendChild(productImgCart); 
+    
+        productInfo.appendChild(productSection);
+        productInfo.appendChild(productFigure);
+    
+        productCard.appendChild(productImg);
+        productCard.appendChild(productInfo);
+    
+        cardsContainer.appendChild(productCard);
+    }
+}
+
+crearArticulos(listaProductos);
